c1bd2d3b7b6163b9c79520817f661122ef752607
Andreas Huber <info@ndhbr.de>
2021-12-03 14:48:04 +0100

    Unicode Bug

--- setup.py
+++ setup.py
@@ -18,61 +18,65 @@ f.close()
 # Add git keys
 ssh_path = "/home/runner/.ssh"
 try:
-  # Create ssh folder
-  if not os.path.isdir(ssh_path):
-    os.mkdir(ssh_path)
+    # Create ssh folder
+    if not os.path.isdir(ssh_path):
+        os.mkdir(ssh_path)
 except OSError:
-  print ("Creation of the directory %s failed" % ssh_path)
+    print("Creation of the directory %s failed" % ssh_path)
 else:
-  # Crypto key
-  key = rsa.generate_private_key(
-      backend=crypto_default_backend(),
-      public_exponent=65537,
-      key_size=2048
-  )
-
-  # Private ssh key
-  private_ssh_key_path = "%s/id_rsa" % ssh_path
-  private_ssh_key = os.getenv("private_ssh_key")
-  if not private_ssh_key:
-    private_ssh_key = key.private_bytes(
-      crypto_serialization.Encoding.PEM,
-      crypto_serialization.PrivateFormat.PKCS8,
-      crypto_serialization.NoEncryption()
+    # Crypto key
+    key = rsa.generate_private_key(
+        backend=crypto_default_backend(),
+        public_exponent=65537,
+        key_size=2048
     )
-    private_ssh_key = private_ssh_key.decode("UTF-8")
-  else:
-    private_ssh_key = private_ssh_key.decode('unicode_escape')
-  print(private_ssh_key)
-  f = open(private_ssh_key_path, "w")
-  f.write(private_ssh_key or "")
-  f.close()
-  os.chmod(private_ssh_key_path, 0o600)
 
-  # Public ssh key
-  public_ssh_key_path = "%s/id_rsa.pub" % ssh_path
-  public_ssh_key = os.getenv("public_ssh_key")
-  if not public_ssh_key:
-    public_ssh_key = key.public_key().public_bytes(
-      crypto_serialization.Encoding.OpenSSH,
-      crypto_serialization.PublicFormat.OpenSSH
-    )
-    public_ssh_key = public_ssh_key.decode("utf-8")
-  else:
-    public_ssh_key = public_ssh_key.decode('unicode_escape')
-  f = open(public_ssh_key_path, "w")
-  f.write(public_ssh_key or "")
-  f.close()
-  os.chmod(public_ssh_key_path, 0o600)
+    # Private ssh key
+    private_ssh_key_path = "%s/id_rsa" % ssh_path
+    private_ssh_key = os.getenv("private_ssh_key")
+    if not private_ssh_key:
+        private_ssh_key = key.private_bytes(
+            crypto_serialization.Encoding.PEM,
+            crypto_serialization.PrivateFormat.PKCS8,
+            crypto_serialization.NoEncryption()
+        )
+        private_ssh_key = private_ssh_key.decode("UTF-8")
+    else:
+        private_ssh_key = bytes(private_ssh_key, 'utf-8')
+        private_ssh_key = private_ssh_key.decode('unicode_escape')
+    print(private_ssh_key)
+    f = open(private_ssh_key_path, "w")
+    f.write(private_ssh_key or "")
+    f.close()
+    os.chmod(private_ssh_key_path, 0o600)
 
-  # Environment
-  f = open("%s/.env" %file_path, "w")
-  f.writelines([
-    'public_ssh_key="%s"' % public_ssh_key.encode('unicode_escape'),
-    '\nprivate_ssh_key="%s"' % private_ssh_key.encode('unicode_escape'),
-  ])
-  f.close()
+    # Public ssh key
+    public_ssh_key_path = "%s/id_rsa.pub" % ssh_path
+    public_ssh_key = os.getenv("public_ssh_key")
+    if not public_ssh_key:
+        public_ssh_key = key.public_key().public_bytes(
+            crypto_serialization.Encoding.OpenSSH,
+            crypto_serialization.PublicFormat.OpenSSH
+        )
+        public_ssh_key = public_ssh_key.decode("utf-8")
+    else:
+        public_ssh_key = bytes(public_ssh_key, 'utf-8')
+        public_ssh_key = public_ssh_key.decode('unicode_escape')
+    f = open(public_ssh_key_path, "w")
+    f.write(public_ssh_key or "")
+    f.close()
+    os.chmod(public_ssh_key_path, 0o600)
 
-  # Ready
-  print("Starting OTH-Console...")
-  print("\nYou now can use the commands get, check and submit.")
+    # Environment
+    f = open("%s/.env" % file_path, "w")
+    f.writelines([
+        'public_ssh_key="%s"' % public_ssh_key.encode(
+            'unicode_escape').decode("utf-8"),
+        '\nprivate_ssh_key="%s"' % private_ssh_key.encode(
+            'unicode_escape').decode("utf-8"),
+    ])
+    f.close()
+
+    # Ready
+    print("Starting OTH-Console...")
+    print("\nYou now can use the commands get, check and submit.")
