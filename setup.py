0e4540396ec8239809d678ae0a994ac580110a6e
Andreas Huber <info@ndhbr.de>
2021-12-03 13:43:23 +0100

    Automated generation of ssh keys

--- setup.py
+++ setup.py
@@ -33,43 +33,44 @@ else:
   # Private ssh key
   private_ssh_key_path = "%s/id_rsa" % ssh_path
   private_ssh_key = os.getenv("private_ssh_key")
+  private_ssh_key = None
   if not private_ssh_key:
     private_ssh_key = key.private_bytes(
       crypto_serialization.Encoding.PEM,
       crypto_serialization.PrivateFormat.PKCS8,
       crypto_serialization.NoEncryption()
     )
+    private_ssh_key = private_ssh_key.decode("utf-8")
+    private_ssh_key = private_ssh_key.replace("\n", "")
   f = open(private_ssh_key_path, "w")
-  f.write(private_ssh_key.decode("utf-8") or "")
+  f.write(private_ssh_key or "")
   f.close()
   os.chmod(private_ssh_key_path, 0o600)
 
   # Public ssh key
   public_ssh_key_path = "%s/id_rsa.pub" % ssh_path
   public_ssh_key = os.getenv("public_ssh_key")
+  public_ssh_key = None
   if not public_ssh_key:
-    public_key = key.public_key().public_bytes(
+    public_ssh_key = key.public_key().public_bytes(
       crypto_serialization.Encoding.OpenSSH,
       crypto_serialization.PublicFormat.OpenSSH
     )
+    public_ssh_key = public_ssh_key.decode("utf-8")
+    public_ssh_key = public_ssh_key.replace("\n", "")
   f = open(public_ssh_key_path, "w")
-  f.write(public_key.decode("utf-8") or "")
+  f.write(public_ssh_key or "")
   f.close()
   os.chmod(public_ssh_key_path, 0o600)
 
   # Environment
   f = open("%s/.env" %file_path, "w")
   f.writelines([
-    "public_ssh_key=\"%s\"" % public_ssh_key,
-    "\nprivate_ssh_key=\"%s\"" % private_ssh_key
+    'public_ssh_key="%s"' % public_ssh_key,
+    '\nprivate_ssh_key="%s"' % private_ssh_key,
   ])
   f.close()
 
+  # Ready
   print("Starting OTH-Console...")
-  # Missing SSH key warning
-  # TODO: we may generate a key pair for the user
-  if not os.getenv("public_ssh_key"):
-    print("Warning: public_ssh_key not set (secrets tab)")
-  if not os.getenv("private_ssh_key"):
-    print("Warning: private_ssh_key not set (secrets tab)")
   print("\nYou now can use the commands get, check and submit.")
